0ffa01435f4ee408c9a0751c1e05a17744a3de34
Austen LeBeau <austenlebeau@gmail.com>
2021-02-19 11:35:33 -0600

    customization

--- js/weather.js
+++ js/weather.js
@@ -9,11 +9,11 @@ weather.temperature = {
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = 'C';
+var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '796fe3674c27c9513e0c69c6b7615571';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 34.776523;
+  let longitude = -86.736499;
 
   getWeather(latitude, longitude);
 }
